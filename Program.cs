c164e75098fdd9888504ec2582f835367126f68c
Noriaki <ashatamega@gmail.com>
2020-09-25 21:56:56 +0500

    Update 2

--- Program.cs
+++ Program.cs
@@ -5,11 +5,13 @@ namespace lab2
 {
     class Program
     {
+        static bool check;
         static void Main(string[] args)
         {
-            bool check;
             do
             {
+                Console.WriteLine("Введите название фигуры");
+                string figure = Console.ReadLine();
                 check = false;
                 Console.WriteLine("Введите начальные и конечные координаты через пробел");
                 int x1 = Convert.ToInt32(Convert.ToChar(Console.Read()));
@@ -19,38 +21,84 @@ namespace lab2
                 int y2 = Convert.ToInt32(Convert.ToChar(Console.Read()));
                 if (IsCoordinatesCorrect(x1, y1, x2, y2))
                 {
-                    IsHorseCorrect(x1, y1, x2, y2);
+                    if (SelectFigure(figure, x1, y1, x2, y2))
+                    {
+                        Console.WriteLine("Верно");
+                    }
+                    else
+                    {
+                        check = true;
+                        Console.WriteLine("Неверно");
+                    }
                 }
                 else
                 {
+                    Console.WriteLine("Выбранные вами координаты не соответствуют игровому полю");
                     check = true;
                 }
                 //Console.WriteLine($"{x1} {y1} {x2} {y2}");
             } while (check);
         }
-        static void IsHorseCorrect(int x1, int y1, int x2, int y2)
+        static bool IsHorseCorrect(int x1, int y1, int x2, int y2)
         {
-            if ((((x2 == x1--) || (x2 == x1++)) && ((y2 == y1 + 2) || (y2 == y1 - 2)))
-            || (((y2 == y1--) || (y2 == y1++)) && ((x2 == x1 + 2) || (x2 == x1 - 2))))
-            {
-                Console.WriteLine("Верно");
-            }
-            else
-            {
-                Console.WriteLine("Неверно");
-            }
+            return ((((x2 == x1--) || (x2 == x1++)) && ((y2 == y1 + 2) || (y2 == y1 - 2)))
+            || (((y2 == y1--) || (y2 == y1++)) && ((x2 == x1 + 2) || (x2 == x1 - 2))));
         }
         static bool IsCoordinatesCorrect(int x1, int y1, int x2, int y2)
         {
-            if (!(x1 > 64 && x1 < 73 && x2 > 64 && x2 < 73 && y1 > 48 && y1 < 57 && y2 > 48 && y2 < 57))
+            return (x1 > 64 && x1 < 73 && x2 > 64 && x2 < 73 && y1 > 48 && y1 < 57 && y2 > 48 && y2 < 57);
+        }
+        static bool IsKingCorrect(int x1, int y1, int x2, int y2)
+        {
+            return (((x2 == x1++) || (x2 == x1--)) && ((y2 == y1) || (y2 == y1--) || (y2 == y1++)))
+            || (((x2 == x1++) || (x2 == x1--) || (x2 == x1)) && ((y2 == y1--) || (y2 == y1++)));
+        }
+        static bool IsQueenCorrect(int x1, int y1, int x2, int y2)
+        {
+            return ((x2 == x1) && (y2 != y1)) || ((y2 == y1) && (x2 != x1)) || (Math.Abs(x2 - x1) == Math.Abs(y2 - y1));
+        }
+        static bool IsBishopCorrect(int x1, int y1, int x2, int y2)
+        {
+            return ((Math.Abs(x2 - x1) == Math.Abs(y2 - y1)) && x2 != x1 && y2 != y1);
+        }
+        static bool IsRockCorrect(int x1, int y1, int x2, int y2)
+        {
+            return ((x2 == x1) && (y2 != y1)) || ((y2 == y1) && (x2 != x1));
+        }
+        static bool IsPawnCorrect(int x1, int y1, int x2, int y2)
+        {
+            return (x2 == x1) && ((y2 == y1++) || (y2 == y1--));
+        }
+        static bool SelectFigure(string figure, int x1, int y1, int x2, int y2)
+        {
+            if (figure == "Король")
             {
-                Console.WriteLine("Выбранные вами координаты не являются частью поля");
-                Console.WriteLine("Попробуйте ещё раз");
-                return false;
+                return IsKingCorrect(x1, y1, x2, y2);
+            }
+            else if (figure == "Ферзь")
+            {
+                return IsQueenCorrect(x1, y1, x2, y2);
+            }
+            else if (figure == "Слон")
+            {
+                return IsBishopCorrect(x1, y1, x2, y2);
+            }
+            else if (figure == "Конь")
+            {
+                return IsHorseCorrect(x1, y1, x2, y2);
+            }
+            else if (figure == "Ладья")
+            {
+                return IsRockCorrect(x1, y1, x2, y2);
+            }
+            else if (figure == "Пешка")
+            {
+                return IsPawnCorrect(x1, y1, x2, y2);
             }
             else
             {
-                return true;
+                Console.WriteLine("Вы ввели несуществующую фигуру");
+                return false;
             }
         }
     }
